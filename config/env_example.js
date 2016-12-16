393b50135a6590216e5983a5068a501555a2425a
Ling Yu Chen <lingchen215@gmail.com>
2016-12-16 13:39:01 -0800

    Add firebase env keys config template

---
+++ config/env_example.js
@@ -0,0 +1,10 @@
+//replace following with the Firebase configs, then change the filename to env.js
+const config = {
+  apiKey: "your apiKey",
+  authDomain: "your authDomain",
+  databaseURL: "your databaseURL",
+  storageBucket: "your storageBucket",
+  messagingSenderId: "your messagingSenderId"
+};
+
+export default config;

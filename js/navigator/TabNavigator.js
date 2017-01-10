2803c0dd7df6c0b4650be84ff63c9f0fca4f078c
Ling Yu Chen <lingchen215@gmail.com>
2017-01-10 15:20:06 -0800

    Change cardStack to Transitioner for tab navigator
    Currently when a tab is switch, the navigator animates all the
    tabs in between the origin and destination tabs, this is rather
    unpleasant to see and is the expected behavior of cardStack,
    we change cardStack to NavigationTransitioner so that the animation
    can be fix in the future.

--- js/navigator/TabNavigator.js
+++ js/navigator/TabNavigator.js
@@ -3,8 +3,7 @@
  */
 
 import React, { Component } from 'react';
-import { NavigationExperimental, View, StatusBar } from 'react-native';
-const { CardStack } = NavigationExperimental;
+import { Animated, NavigationExperimental, View, StatusBar, StyleSheet } from 'react-native';
 import LOCAL_ROUTES from '../navigator/RoutesLocal.js';
 import HeaderLocal from '../header/HeaderLocal.js';
 import TabBar from '../tabs/TabBar.js';
@@ -12,6 +11,9 @@ import { pop } from '../actions/navigation.js';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Firebase from 'firebase';
+
+const { Card, Transitioner } = NavigationExperimental;
+const { PagerStyleInterpolator } = Card;
 
 //tabs are rendered when there is only one navigator handling all the tabs
 //we set up a dedicated navigator for each tab so each tab will not get rerender when tab switches
@@ -26,8 +28,10 @@ import {
 class TabNavigator extends Component {
   constructor(props) {
     super(props);
-    this.FitlyFirebase = this.props.FitlyFirebase;
-    this.userUpdateRef = this.FitlyFirebase.database().ref(`/userUpdatesMajor/${this.props.uID}`);
+    this.database = this.props.FitlyFirebase.database();
+    this.userUpdateRef = this.database.ref(`/userUpdatesMajor/${this.props.uID}`);
+
+    this._render = this._render.bind(this);
   };
 
   componentDidMount() {
@@ -62,41 +66,70 @@ class TabNavigator extends Component {
     this.userUpdateRef.off('child_added');
   };
 
+  _render(transitionProps) {
+    const scenes = transitionProps.scenes.map((scene) => {
+      const sceneProps = {
+        ...transitionProps,
+        scene,
+      };
+      return this._renderScene(sceneProps);
+    });
+
+    return (
+      <View style={{flex: 1}}>
+        {scenes}
+      </View>
+    );
+  }
 
   _renderScene(sceneProps) {
-    if (!this.props.isLoggedIn) {
-      return (<View></View>);
-    }
+    console.log('asdfasd');
+    const style = [
+      styles.scene,
+      PagerStyleInterpolator.forHorizontal(sceneProps),
+    ];
+
+    let Scene;
     const {key} = sceneProps.scene.route;
-    if (key === 'Activity') {
-      return <ActivityNavigator/>
-    } else if (key === 'Search') {
-      return <SearchNavigator/>
-    } else if (key === 'Profile') {
-      return <ProfileNavigator/>
-    } else if (key === 'Notification') {
-      return <NotificationNavigator/>
-    } else if (key === 'Connect') {
-      return <ConnectNavigator/>
-    }
+    if (!this.props.isLoggedIn) { Scene = (<View></View>) }
+    else if (key === 'Activity') { Scene = ActivityNavigator }
+    else if (key === 'Search') { Scene = SearchNavigator }
+    else if (key === 'Profile') { Scene = ProfileNavigator }
+    else if (key === 'Notification') { Scene = NotificationNavigator }
+    else if (key === 'Connect') { Scene = ConnectNavigator }
+
+    return  (
+      <Animated.View style={style} key={sceneProps.scene.key} >
+        <Scene {...this.sceneProps} style={style} />
+      </Animated.View>
+    );
   };
+
 
   render() {
     return (
       <View style={{flex: 1}}>
-        <StatusBar
-          barStyle="light-content"
-        />
-        <CardStack
-          key={this.props.navState.tabs.key}
-          navigationState={this.props.navState.tabs}
-          renderScene={this._renderScene.bind(this)}
+        <StatusBar barStyle="light-content"/>
+        <Transitioner
+         navigationState={this.props.navState.tabs}
+         render={this._render}
         />
         <TabBar/>
       </View>
     )
    }
  };
+
+ const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+});
 
  const mapStateToProps = function(state) {
   return {
